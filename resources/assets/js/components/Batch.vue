2d4f22801f063fb286a6d726c0c12b170a2fc5a9
Alexandre Roy <a-roy13@orange.fr>
2017-10-25 15:39:01 +0200

    warning on batch + removed comments and spacing

--- resources/assets/js/components/Batch.vue
+++ resources/assets/js/components/Batch.vue
@@ -1,22 +1,22 @@
 <template>
     <section>
         <div class="jumbotron">
-            <!--<div class="form-group" v-for="el in elements">-->
-                <!--<alert type="success" :message="el.message" v-if="el.success == true"></alert>-->
-                <!--<alert type="danger" :message="el.message" v-if="el.success == false"></alert>-->
-            <!--</div>-->
             <ul class="list-group">
                 <template v-for="el in elements">
-                <li class="list-group-item list-group-item-success" v-if="el.success == true">{{el.message}}</li>
-                <li class="list-group-item list-group-item-danger" v-if="el.success == false">{{el.message}}</li>
+                    <li class="list-group-item list-group-item-success" v-if="el.success == true">{{el.message}}</li>
+                    <li class="list-group-item list-group-item-danger" v-if="el.success == false">{{el.message}}</li>
                 </template>
             </ul>
             <h1 style="color: white;">Batch email verification</h1>
-
             <form action="/checkbatch" method="post" @submit.prevent="checkEmails"
                   @keydown="clearError($event.target.name)">
-
-                <div :class="['form-group', errors ? 'has-error hasfeedback' : '' ]">
+                <div class="alert alert-warning alert-dismissible" role="alert">
+                    <button type="button" class="close" data-dismiss="alert" aria-label="Close">
+                        <span aria-hidden="true">&times;</span>
+                    </button>
+                    <strong>Warning!</strong> You must enter comma separated emails with no spaces.
+                </div>
+                <div class="form-group">
                     <label class="control-label" for="emails" v-if="errors">{{ errors.toString() }}</label>
                     <textarea type="text" name="emails" class="form-control" id="emails" aria-describedby="emailStatus"
                               v-model="check.emails" required="required" :disabled="disabled" rows="5">
@@ -25,12 +25,9 @@
                           aria-hidden="true"></span>
                     <span id="emailStatus" class="sr-only">(error)</span>
                 </div>
-
                 <button type="submit" class="btn btn-lg btn-success" role="button" :disabled="disabled">Check</button>
             </form>
-
         </div>
-
     </section>
 </template>
 
